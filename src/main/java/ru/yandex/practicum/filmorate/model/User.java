aafb314731bb482bd71ae13c16b08dbe7beffff1
Vera_Z <zhenih-v@mail.ru>
2024-04-19 22:16:39 +0300

    ТЗ 10 вторая версия. Исключен неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import annotations.NotSpase;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.Data;
 
@@ -15,7 +14,7 @@ public class User {
     @NotBlank
     @Email
     private String email;
-   @NotBlank
+    @NotBlank
     @NotSpase
     private String login;
     private String name;
