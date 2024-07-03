7bcd294eac6a0911dbe1c5b93e4fc7b26ec6b3fc
mrchcat <m-cat@mail.ru>
2024-07-03 13:33:04 +0300

    feat: tests

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -4,24 +4,74 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@ExtendWith(MockitoExtension.class)
 class FilmControllerTest {
 
     private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    private MockMvc mockMvc;
 
     @AllArgsConstructor
     static class ExpectedViolation {
         String propertyPath;
         String message;
     }
+
+    @Mock
+    private FilmService filmService;
+
+    @InjectMocks
+    private FilmController filmController;
+
+    @BeforeEach
+    void setMockMvc() {
+        mockMvc = MockMvcBuilders.standaloneSetup(filmController).build();
+    }
+
+    @Test
+    @DisplayName("check controller for common films with current parameters")
+    void correctParametresForCommonFilmsControllerTest() throws Exception {
+        Long userId = 12L;
+        Long friendId = 18L;
+        String request = String.format("/films/common?userId=%d&friendId=%d", userId, friendId);
+        mockMvc.perform(get(request));
+        Mockito.verify(filmService).getCommonFilms(userId, friendId);
+    }
+
+    @Test
+    @DisplayName("check controller for common films with bad parameters")
+    void badParametresForCommonFilmsControllerTest() throws Exception {
+        Long userId = -12L;
+        Long friendId = 18L;
+        String request = String.format("/films/common?userId=%d&friendId=%d", userId, friendId);
+        mockMvc.perform(get(request)).andExpect(
+                result -> assertInstanceOf(HandlerMethodValidationException.class, result.getResolvedException()));
+    }
+
 
     @Test
     void validateFilmOk() {
@@ -158,4 +208,6 @@ class FilmControllerTest {
         assertEquals(expectedViolation.message, violations.get(0).getMessage());
         assertEquals(1, violations.size());
     }
+
+
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class FilmServiceTest {
+    @Mock
+    FilmStorage filmStorage;
+
+    @InjectMocks
+    FilmService filmService;
+
+    @Test
+    @DisplayName("test common film")
+    void getCommonFilms() {
+        Long userId= 12L;
+        Long friendId=18L;
+        filmService.getCommonFilms(userId,friendId);
+        Mockito.verify(filmStorage).getCommonFilms(userId,friendId);
+    }
+}
