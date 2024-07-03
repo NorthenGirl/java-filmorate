440b0be4dc7c025990bcced49e12b0673a353b4f
Ycseeasy <ycse2easyy@yandex.ru>
2024-07-03 21:07:52 +0300

    Поменял немного логику согласно комментариям

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
-import java.util.Set;
 
 public interface FilmStorage {
 
