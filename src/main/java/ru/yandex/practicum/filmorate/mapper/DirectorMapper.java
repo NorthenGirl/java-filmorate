9109134c9ef01f4e10722082704ac0b79412dfda
NorthenGirl <153527933+NorthenGirl@users.noreply.github.com>
2024-07-08 17:06:26 +0300

    Fix director mapper (#60)
    * fix: director mapper
    
    * fix: checkstyle
    
    ---------
    
    Co-authored-by: mrchcat <m-cat@mail.ru>

--- src/main/java/ru/yandex/practicum/filmorate/mapper/DirectorMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/DirectorMapper.java
@@ -6,26 +6,13 @@ import ru.yandex.practicum.filmorate.model.Director;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class DirectorMapper implements RowMapper<Director> {
-    private Map<Long, Director> directors = new HashMap<>();
-
     public Director mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Long id = rs.getLong("id");
-        Director director = directors.get(id);
-        if (director == null) {
-            director = Director.builder()
-                    .id(id)
-                    .name(rs.getString("name"))
-                    .build();
-            director.setId(id);
-        }
-        if (rs.isLast()) {
-            directors.clear();
-        }
-        return director;
+        return Director.builder()
+                .id(rs.getLong("id"))
+                .name(rs.getString("name"))
+                .build();
     }
 }
