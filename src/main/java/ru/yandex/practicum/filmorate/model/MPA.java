230160a3aff009b1f286debfecfaf9bf4b622d1d
mrchcat <m-cat@mail.ru>
2024-07-08 13:46:55 +0300

    fix: tests and MPA annotation

--- src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
+@ToString
 public class MPA {
     private Long id;
     private String name;
